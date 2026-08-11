8186c66b0f4ce09cd5ae0572abef47e125dd8bf4
derrickatsu1998 <102452843+derrickatsu1998@users.noreply.github.com>
2026-08-10 17:58:47 -0700

    Add GDAL installation to build script

--- build.sh
+++ build.sh
@@ -4,12 +4,7 @@ set -o errexit
 # Install GDAL and GEOS (required for GeoDjango)
 apt-get update && apt-get install -y gdal-bin libgdal-dev
 
-# Install Python dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# Collect static files
 python manage.py collectstatic --no-input
-
-# Run migrations
 python manage.py migrate
