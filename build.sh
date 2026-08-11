1c8e395d328bf8ed9f4a0222ac846c68bbf85c4c
derrickatsu1998 <102452843+derrickatsu1998@users.noreply.github.com>
2026-08-10 18:33:55 -0700

    Remove Python gdal package, use system GDAL only

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Add GDAL repository for newer version
-apt-get update
-apt-get install -y software-properties-common
-add-apt-repository ppa:ubuntugis/ubuntugis-unstable
 apt-get update
 apt-get install -y gdal-bin libgdal-dev
-
-export GDAL_CONFIG=/usr/bin/gdal-config
 
 pip install --upgrade pip
 pip install -r requirements.txt
