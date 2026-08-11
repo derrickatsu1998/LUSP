8e6f16efc76088117f26991beb27588d1b69bdaf
derrickatsu1998 <102452843+derrickatsu1998@users.noreply.github.com>
2026-08-10 19:07:26 -0700

    Update build.sh with verbose logging

--- build.sh
+++ build.sh
@@ -3,11 +3,6 @@ set -o errexit
 
 echo "=== Starting build.sh ==="
 
-echo "=== Installing system GDAL ==="
-apt-get update
-apt-get install -y gdal-bin libgdal-dev
-echo "=== GDAL installation complete ==="
-
 echo "=== Installing Python dependencies ==="
 pip install --upgrade pip
 pip install -r requirements.txt
